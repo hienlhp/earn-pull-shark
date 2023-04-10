bd94ee9916816570084dcc2fa32040bfeaf6e34e
Quang Hiển <73958860+HyrniT@users.noreply.github.com>
2023-04-10 10:10:32 +0700

    Update chanqua.cpp

--- chanqua.cpp
+++ chanqua.cpp
@@ -2,6 +2,19 @@
 
 using namespace std;
 
+void Swap(int &a, int &b){
+    int temp = a;
+    a = b;
+    b = temp;
+}
+
+void InterchangeSort(int a[], int n){	
+    for (int i = 0; i < n - 1; i++)
+        for (int j = i + 1; j < n; j++)
+	        if(a[i] > a[j])  //nếu có nghịch thế thì đổi chỗ
+		        Swap(a[i], a[j]);
+}
+
 int main() {
   return 0;
 }
